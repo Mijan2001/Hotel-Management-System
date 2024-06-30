589fc8c3ad41ed629be53577a9615d58edb3c4e3
Mijan2001 <mijan.cse19@gmail.com>
2024-07-01 00:22:56 +0600

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
+
+// vite.config.js
+// export default {
+//   build: {
+//     outDir: "dist",
+//   },
+// };
